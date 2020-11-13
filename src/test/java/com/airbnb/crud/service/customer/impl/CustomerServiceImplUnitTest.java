0868fbf6150e1e2e81c31503eed24ec69576ae11
gets-mukul <mukul.prajapati@nobroker.in>
2020-11-14 00:29:17 +0530

    unit test done for bookingService and customerService

--- src/test/java/com/airbnb/crud/service/customer/impl/CustomerServiceImplUnitTest.java
+++ src/test/java/com/airbnb/crud/service/customer/impl/CustomerServiceImplUnitTest.java
@@ -5,16 +5,24 @@ import com.airbnb.crud.airbnbDB.customer.entity.Customer;
 import com.airbnb.crud.airbnbDB.enums.AccountStatus;
 import com.airbnb.crud.airbnbDB.enums.PaymentType;
 import com.airbnb.crud.airbnbDB.enums.PersonGender;
+import com.airbnb.crud.controller.customer.model.CreateCustomerRequest;
+import com.airbnb.crud.controller.customer.model.CustomerDetails;
+import com.airbnb.crud.exceptions.EntityNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
 
 class CustomerServiceImplUnitTest {
-    final Customer customer = Customer.builder()
+
+    final CreateCustomerRequest customerRequest = CreateCustomerRequest.builder()
             .cardNumber("123ABC456")
             .accountStatus(AccountStatus.deactivated.name())
             .birthDate(String.valueOf(new Date().getTime()))
@@ -29,34 +37,63 @@ class CustomerServiceImplUnitTest {
             .photo("some_photo_url")
             .securityCode("!@#$%^&*()")
             .build();
+
+    private CustomerServiceImpl customerService;
     private CustomerDaoImpl customerDao;
 
     @BeforeEach
     public void setup() {
         customerDao = Mockito.mock(CustomerDaoImpl.class);
+        customerService = new CustomerServiceImpl(customerDao);
     }
 
     @Test
     @DisplayName("Create customer for null input")
     public void createCustomer_with_null_input() {
-        try {
-            customerDao.createCustomer(null);
-        } catch (final Exception e) {
-            Assertions.fail("Test case fail while creating null customer", e);
-        }
+        Assertions.assertThrows(NullPointerException.class, () -> {
+            customerService.createCustomer(null);
+        });
     }
 
     @Test
     @DisplayName("Success test case for create customer")
     public void createCustomer_with_success_input() {
         try {
-            customerDao.createCustomer(customer);
+            customerService.createCustomer(customerRequest);
         } catch (final Exception e) {
             Assertions.fail("Test case fail for creating of customer", e);
         }
     }
 
     @Test
-    void getCustomersForCity() {
+    @DisplayName("Test case for null city value")
+    public void getCustomersForCity_for_null_city_value() {
+        Assertions.assertThrows(EntityNotFoundException.class, () -> {
+            customerService.getCustomersForCity(null);
+        });
+    }
+
+    @Test
+    @DisplayName("Test case for empty city value")
+    public void getCustomerForCity_for_empty_city_value() {
+        Assertions.assertThrows(EntityNotFoundException.class, () -> {
+            customerService.getCustomersForCity("");
+        });
+    }
+
+    @Test
+    @DisplayName("Test case for valid city value")
+    public void getCustomerForCity_for_valid_city_value() {
+        when(customerDao.getCustomersForCity("Boston")).thenReturn(Collections.singletonList(Customer.builder().build()));
+        try {
+            final List<CustomerDetails> customers = customerService.getCustomersForCity("Boston");
+            if (customers.size() <= 0) {
+                Assertions.fail("Test case failed while fetching customers for valid city");
+            }
+        } catch (final EntityNotFoundException e) {
+            Assertions.fail("Test case failed while fetching customers for valid city", e);
+        } catch (final Exception e) {
+            Assertions.fail("Test case failed while fetching customers for valid city", e);
+        }
     }
 }
